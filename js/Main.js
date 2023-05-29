e56d3390eefb24a5345e22868df8eb11070e6442
ramarfx <ramagamiing33@gmail.com>
2023-05-29 13:58:13 +0700

    aldikun

---
+++ js/Main.js
@@ -0,0 +1,13 @@
+var typing = new Typed(".text", {
+    strings: ["Sehat!", "Penurun Kalori!", "Kesehatan Tubuh!"],
+    typeSpeed: 120,
+    backSpeed: 40,
+   loop: true,
+   });
+
+   var typing = new Typed(".jam1", {
+    strings: ["Web ini hanya untuk menghitung kebutuhan kalori dalam sehari. Jika ingin membeli makanan penurun kalori beli di webnya langsung atau klik di bawah ini jangan sampai lupa dan jangan kebanyak belinya. <br> Supaya lebih efesien dalam penurunan berat badan maka tidurlah yang teratur serta teruslah berolah raga dalam sehari kurangi makanan yang gak sehat buat tubuhmu"],
+    typeSpeed: 14,
+    backSpeed: 10,
+   loop: true,
+   });
